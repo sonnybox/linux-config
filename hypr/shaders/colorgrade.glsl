66fe5eb64b8b1e4d7ccbd69bc84b3be2ec2576b9
Sonny Box <sonny@sonnybox.com>
2024-04-02 01:30:15 -0700

    mostly latest neovim plugin changes

--- hypr/shaders/colorgrade.glsl
+++ hypr/shaders/colorgrade.glsl
@@ -2,13 +2,13 @@ precision highp float;
 varying vec2 v_texcoord;
 uniform sampler2D tex;
 
-const vec3 desaturationFactors = vec3(0.5, 0.5, 0.0);
-const float contrastAmount = 0.98;
-const float brightness = 0.98;
+const vec3 desaturationFactors = vec3(0.5, 0.5, 0.05);
+const float contrastAmount = 0.96;
+const float brightness = 1.00;
 
 void main() {
     vec4 pixColor = texture2D(tex, v_texcoord);
-    vec3 color = vec3(pixColor[0]*0.97, pixColor[1], pixColor[2]) * brightness;
+    vec3 color = vec3(pixColor[0]*0.972, pixColor[1]*0.99, pixColor[2]) * brightness;
 
     // Apply desaturation based on individual RGB channels
     vec3 desaturatedColor;
