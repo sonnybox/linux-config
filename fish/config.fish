e7b44919cce767c8cd52af953a3b0eeae8c04ba6
Sonny Box <imsonnybox@gmail.com>
2025-03-19 16:43:13 -0700

    removed vscode flags added pinentry tty flag

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    pyenv init - fish | source
-    fish_add_path /home/sonny/.local/bin
-    set -x GGML_CUDA_ENABLE_UNIFIED_MEMORY 1
+	fish_add_path /home/sonny/.local/bin
+	set -x GGML_CUDA_ENABLE_UNIFIED_MEMORY 1
+	set -x GPG_TTY $(tty)
 end
+pyenv init - fish | source
